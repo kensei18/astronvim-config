4d3fec7ab68d3d34a6ffc7004d72a62aeadb358a
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-11 10:58:56 +0900

    add mappings for system copy and paste

--- init.lua
+++ init.lua
@@ -89,5 +89,10 @@ return {
     vim.opt.spell = true
     vim.opt.spelloptions = { "camel" }
     vim.opt.spelllang = "en,cjk"
+
+    vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true })
   end,
 }
