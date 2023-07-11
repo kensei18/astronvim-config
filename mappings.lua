88c0cefcafc2b19c6364820e0d66a7ee8e594e50
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-11 10:31:29 +0900

    refactor mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,19 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
+local function incrementFontSize()
+  FontSize = FontSize + 1
+  vim.opt.guifont = { FontName, ":h" .. FontSize }
+  print('increment')
+end
+
+local function decrementFontSize()
+  FontSize = FontSize - 1
+  vim.opt.guifont = { FontName, ":h" .. FontSize }
+  print('decrement')
+end
+
 return {
   -- first key is the mode
   n = {
@@ -29,20 +42,8 @@ return {
     ["<leader>lM"] = { desc = "Markdown" },
     ["<leader>lMp"] = { ":MarkdownPreview<cr>", desc = "Markdown Preview" },
     ["<leader>lMs"] = { ":MarkdownPreviewStop<cr>", desc = "Stop Markdown Preview" },
-    ["<leader>+"] = {
-      function()
-        FontSize = FontSize + 1
-        vim.opt.guifont = { FontName, ":h" .. FontSize }
-        print('increment')
-      end,
-      desc = "Increment Font Size" },
-    ["<leader>-"] = {
-      function()
-        FontSize = FontSize - 1
-        vim.opt.guifont = { FontName, ":h" .. FontSize }
-        print('decrement')
-      end,
-      desc = "Decrement Font Size" },
+    ["<leader>+"] = { incrementFontSize, desc = "Increment Font Size" },
+    ["<leader>-"] = { decrementFontSize, desc = "Decrement Font Size" },
   },
   t = {
     -- setting a mapping to false will disable it
