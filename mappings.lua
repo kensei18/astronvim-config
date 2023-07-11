132b336aa3a56e091c0829dd4112720379e470b9
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-11 22:52:24 +0900

    add octo mappings

--- mappings.lua
+++ mappings.lua
@@ -44,6 +44,22 @@ return {
     ["<leader>fT"] = { ":TodoTelescope<cr>", desc = "Find TODO" },
     ["]T"] = { require('todo-comments').jump_next, desc = "Next TODO" },
     ["[T"] = { require('todo-comments').jump_prev, desc = "Previous TODO" },
+    -- Github
+    ["<leader>G"] = { name = " Github" },
+    ["<leader>Gh"] = { name = "HELP" },
+    ["<leader>Ghi"] = { ":help octo-commands-issue<cr>", desc = "Issue" },
+    ["<leader>Ghp"] = { ":help octo-commands-pr<cr>", desc = "Pull Request" },
+    ["<leader>Gi"] = { ":Octo issue ", desc = " Issue" },
+    -- ["<leader>Gib"] = { ":Octo issue browser<cr>", desc = "Open in browser" },
+    -- ["<leader>Gic"] = { ":Octo issue create<cr>", desc = "Create" },
+    -- ["<leader>Gie"] = { ":Octo issue edit<cr>", desc = "Create" },
+    -- ["<leader>Gil"] = { ":Octo issue list<cr>", desc = "List" },
+    -- ["<leader>Gir"] = { ":Octo issue reload<cr>", desc = "Reload" },
+    ["<leader>Gp"] = { ":Octo pr ", desc = " Pull Request" },
+    -- ["<leader>Gpb"] = { ":Octo pr browser<cr>", desc = "Open in browser" },
+    -- ["<leader>Gpc"] = { ":Octo pr create<cr>", desc = "Create" },
+    -- ["<leader>GpC"] = { ":Octo pr close<cr>", desc = "Close" },
+    -- ["<leader>Gpl"] = { ":Octo pr list<cr>", desc = "List" },
   },
   t = {
     -- setting a mapping to false will disable it
