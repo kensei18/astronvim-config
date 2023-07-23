e6ab34856e58b87daeec9a70b0257a00ebba8a03
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-24 01:15:30 +0900

    add session keymap

--- mappings.lua
+++ mappings.lua
@@ -25,14 +25,15 @@ return {
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr)
-            require("astronvim.utils.buffer").close(bufnr)
-          end
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
         )
       end,
       desc = "Pick to close",
     },
     ["<leader>b"] = { name = "Buffers" },
+
+    -- sessions
+    ["<leader><leader>s"] = { ":SessionManager load_session<cr>", desc = "Load a session" },
 
     -- font size
     ["<leader>+"] = { incrementFontSize, desc = "Increment Font Size" },
@@ -49,8 +50,8 @@ return {
 
     -- todo-comments
     ["<leader>fT"] = { ":TodoTelescope<cr>", desc = "Find TODO" },
-    ["]o"] = { require('todo-comments').jump_next, desc = "Next TODO" },
-    ["[o"] = { require('todo-comments').jump_prev, desc = "Previous TODO" },
+    ["]o"] = { require("todo-comments").jump_next, desc = "Next TODO" },
+    ["[o"] = { require("todo-comments").jump_prev, desc = "Previous TODO" },
 
     -- neotest
     ["<leader><leader>t"] = { name = "Test" },
@@ -60,27 +61,27 @@ return {
         require("neotest").run.run()
         require("neotest").summary.open()
       end,
-      desc = "Run nearest test"
+      desc = "Run nearest test",
     },
 
     -- refactoring
-    ["<leader><leader>r"] = { name = 'Refactoring' },
-    ["<leader><leader>ri"] = { ":Refactor inline_var<cr>", desc = 'Inline var' },
-    ["<leader><leader>rb"] = { ":Refactor extract_block ", desc = 'Extract block' },
-    ["<leader><leader>rf"] = { ":Refactor extract_block_to_file ", desc = 'Extract block to file' },
+    ["<leader><leader>r"] = { name = "Refactoring" },
+    ["<leader><leader>ri"] = { ":Refactor inline_var<cr>", desc = "Inline var" },
+    ["<leader><leader>rb"] = { ":Refactor extract_block ", desc = "Extract block" },
+    ["<leader><leader>rf"] = { ":Refactor extract_block_to_file ", desc = "Extract block to file" },
 
     -- Github
-    ["<leader>G"] = { name = " Github" },
-    ["<leader>Gh"] = { name = "HELP" },
-    ["<leader>Ghi"] = { ":help octo-commands-issue<cr>", desc = "Issue" },
-    ["<leader>Ghp"] = { ":help octo-commands-pr<cr>", desc = "Pull Request" },
-    ["<leader>Gi"] = { ":Octo issue ", desc = " Issue" },
+    -- ["<leader>G"] = { name = " Github" },
+    -- ["<leader>Gh"] = { name = "HELP" },
+    -- ["<leader>Ghi"] = { ":help octo-commands-issue<cr>", desc = "Issue" },
+    -- ["<leader>Ghp"] = { ":help octo-commands-pr<cr>", desc = "Pull Request" },
+    -- ["<leader>Gi"] = { ":Octo issue ", desc = " Issue" },
     -- ["<leader>Gib"] = { ":Octo issue browser<cr>", desc = "Open in browser" },
     -- ["<leader>Gic"] = { ":Octo issue create<cr>", desc = "Create" },
     -- ["<leader>Gie"] = { ":Octo issue edit<cr>", desc = "Create" },
     -- ["<leader>Gil"] = { ":Octo issue list<cr>", desc = "List" },
     -- ["<leader>Gir"] = { ":Octo issue reload<cr>", desc = "Reload" },
-    ["<leader>Gp"] = { ":Octo pr ", desc = " Pull Request" },
+    -- ["<leader>Gp"] = { ":Octo pr ", desc = " Pull Request" },
     -- ["<leader>Gpb"] = { ":Octo pr browser<cr>", desc = "Open in browser" },
     -- ["<leader>Gpc"] = { ":Octo pr create<cr>", desc = "Create" },
     -- ["<leader>GpC"] = { ":Octo pr close<cr>", desc = "Close" },
@@ -90,11 +91,11 @@ return {
     ["<leader><leader>"] = { name = "Next Menu" },
 
     -- refactoring
-    ["<leader><leader>r"] = { name = 'Refactoring' },
-    ["<leader><leader>ri"] = { ":Refactor inline_var<cr>", desc = 'Inline var' },
-    ["<leader><leader>re"] = { ":Refactor extract ", desc = 'Extract' },
-    ["<leader><leader>rv"] = { ":Refactor extract_var ", desc = 'Extract var' },
-    ["<leader><leader>rf"] = { ":Refactor extract_to_file ", desc = 'Extract to file' },
+    ["<leader><leader>r"] = { name = "Refactoring" },
+    ["<leader><leader>ri"] = { ":Refactor inline_var<cr>", desc = "Inline var" },
+    ["<leader><leader>re"] = { ":Refactor extract ", desc = "Extract" },
+    ["<leader><leader>rv"] = { ":Refactor extract_var ", desc = "Extract var" },
+    ["<leader><leader>rf"] = { ":Refactor extract_to_file ", desc = "Extract to file" },
   },
   t = {
     -- setting a mapping to false will disable it
