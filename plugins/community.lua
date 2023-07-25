15bf3624f80486250d40660ba2b4877caaba161a
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-25 13:40:19 +0900

    update color scheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,4 +5,10 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.sonokai" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.fluormachine-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
 }
