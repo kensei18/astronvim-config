28c76e40c7550f20691455e97afaeadecfd4ec7b
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-25 12:42:28 +0900

    add autotag setting to treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,14 +5,17 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
     })
+    opts.autotag = {
+      enable = true,
+    }
     opts.incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = '<CR>',
-        scope_incremental = '<CR>',
-        node_incremental = '<TAB>',
-        node_decremental = '<S-TAB>',
-      }
+        init_selection = "<CR>",
+        scope_incremental = "<CR>",
+        node_incremental = "<TAB>",
+        node_decremental = "<S-TAB>",
+      },
     }
   end,
 }
