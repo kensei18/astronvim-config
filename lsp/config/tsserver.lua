0f51f5217b2c34be1a7710c2bee16596676da468
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-28 19:00:35 +0900

    add tsserver config

---
+++ lsp/config/tsserver.lua
@@ -0,0 +1,22 @@
+local commonSettings = {
+  format = {
+    insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = false,
+  },
+  inlayHints = {
+    includeInlayEnumMemberValueHints = true,
+    includeInlayFunctionLikeReturnTypeHints = true,
+    includeInlayFunctionParameterTypeHints = true,
+    includeInlayParameterNameHints = "all",
+    includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+    includeInlayPropertyDeclarationTypeHints = true,
+    includeInlayVariableTypeHints = true,
+    includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+  },
+}
+
+return {
+  settings = {
+    javascript = commonSettings,
+    typescript = commonSettings,
+  },
+}
