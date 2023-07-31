98f44b776e76706d16d9f57e5e8e1920c2f2756d
Kensei Sakai <kensei.sakai@gmail.com>
2023-07-31 09:48:09 +0900

    add yamlls config

---
+++ lsp/config/yamlls.lua
@@ -0,0 +1,11 @@
+return {
+  settings = {
+    yaml = {
+      schemas = {
+        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.*{yml,yaml}",
+      },
+    },
+  },
+}
