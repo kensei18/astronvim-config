79fee0e00f6ad404daf276f4a975de98cd443595
kensei18 <kensei.sakai@gmail.com>
2023-08-06 13:30:59 +0900

    update json and yaml schema support

---
+++ lsp/config/jsonls.lua
@@ -0,0 +1,9 @@
+return {
+  settings = {
+    json = {
+      schemas = {
+        { fileMatch = { ".graphqlrc.json" }, url = "https://unpkg.com/graphql-config/config-schema.json" },
+      },
+    },
+  },
+}
