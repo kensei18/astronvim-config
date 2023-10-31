ee1cd272199d26c857ef9e9a62886bbd90399f9e
Kensei Sakai <kensei.sakai@gmail.com>
2023-10-31 17:50:03 +0900

    remove unused mapping

--- mappings.lua
+++ mappings.lua
@@ -31,14 +31,6 @@ local maps = {
 
     -- buffer
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<leader>bD"] = {
-      function()
-        require("astronvim.utils.status").heirline.buffer_picker(
-          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
-        )
-      end,
-      desc = "Pick to close",
-    },
     ["<leader>b"] = { name = "Buffers" },
 
     -- sessions
