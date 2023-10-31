9bc8be7b910182183a8253633e2a00ed03246994
Kensei Sakai <kensei.sakai@gmail.com>
2023-10-31 17:48:06 +0900

    add rainbow_csv plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -66,4 +66,23 @@ return {
     event = "VeryLazy",
     build = ":DirtytalkUpdate",
   },
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
+  },
 }
