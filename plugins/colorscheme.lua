24255f4d8041945b7cfc4c6a052ffffe959aa50a
kensei.sakai <kensei.sakai@lpc00001.local>
2023-12-13 14:12:47 +0900

    stop using astrocommunity for colorscheme

---
+++ plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    init = function()
+      require("kanagawa").setup {
+        -- transparent = true,
+        -- dimInactive = true,
+      }
+    end,
+  },
+}
--- plugins/community.lua
+++ plugins/community.lua
@@ -4,12 +4,5 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.dracula-nvim" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.sonokai" },
-  { import = "astrocommunity.colorscheme.github-nvim-theme" },
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  { import = "astrocommunity.colorscheme.fluormachine-nvim" },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  -- { import = "astrocommunity.colorscheme.kanagawa-nvim" },
 }
