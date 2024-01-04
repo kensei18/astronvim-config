a61d2df24b5a8f6c2cd2291f022704a5f7783e42
kensei18 <kensei.sakai@lpc00001.local>
2024-01-04 15:56:11 +0900

    add ChatGPT.nvim plugin

--- plugins/git.lua
+++ plugins/git.lua
@@ -1,16 +1,4 @@
 return {
-  -- {
-  --   "github/copilot.vim",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     vim.g.copilot_no_tab_map = true
-  --     vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-  --     vim.api.nvim_set_keymap("i", "<C-j>", "<Plug>(copilot-next)", {})
-  --     vim.api.nvim_set_keymap("i", "<C-k>", "<Plug>(copilot-previous)", {})
-  --     vim.api.nvim_set_keymap("i", "<C-h>", "<Plug>(copilot-dismiss)", {})
-  --     vim.api.nvim_set_keymap("i", "<C-i>", "<Plug>(copilot-suggest)", {})
-  --   end,
-  -- },
   {
     "tyru/open-browser-github.vim",
     dependencies = {
