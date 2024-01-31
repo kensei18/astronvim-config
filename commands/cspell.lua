c82fef29c3f3b72ca6d2a31229127d8d1811e071
kensei18 <kensei.sakai@gmail.com>
2024-01-31 19:00:09 +0900

    update cspell command

--- commands/cspell.lua
+++ commands/cspell.lua
@@ -17,7 +17,14 @@ function M.append()
   if word == "" then return end
   print(word)
 
-  io.popen("echo " .. word .. " >> " .. os.getenv "XDG_DATA_HOME" .. "/cspell/user.txt")
+  local data_dir = ""
+  local xdg_data_home = os.getenv "XDG_DATA_HOME"
+  if xdg_data_home ~= nil and xdg_data_home ~= "" then
+    data_dir = xdg_data_home
+  else
+    data_dir = "~/.local/share/"
+  end
+  io.popen("echo " .. word .. " >> " .. data_dir .. "/cspell/user.txt")
   vim.notify('"' .. word .. '" is appended to user dictionary', vim.log.levels.INFO)
 
   if vim.api.nvim_get_option_value("modifiable", {}) then
