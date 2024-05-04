feecc68b2eaf5272a91b45fe3285279feb3e00e3
kensei18 <kensei.sakai@gmail.com>
2024-05-04 16:54:28 +0900

    update resession hooks

--- lua/plugins/resession.lua
+++ lua/plugins/resession.lua
@@ -4,6 +4,9 @@ return {
     opts = function()
       local resession = require "resession"
       resession.add_hook("post_load", function() vim.cmd "Neotree dir=./" end)
+      resession.add_hook("pre_load", function()
+        if vim.bo.filetype ~= "alpha" then resession.save(vim.fn.getcwd(), { dir = "dirsession" }) end
+      end)
     end,
   },
 }
