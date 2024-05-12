fba6d94c0d1206842ae46ebbdb9ced32408eec38
kensei18 <kensei.sakai@gmail.com>
2024-05-12 20:30:15 +0900

    add prettier config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -32,11 +32,24 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
+        "prettierd",
         "stylua",
         "goimports",
         "cspell",
       })
+
+      opts.handlers = {
+        prettierd = function()
+          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+            condition = function(utils)
+              return utils.root_has_file "package.json"
+                or utils.root_has_file ".prettierrc"
+                or utils.root_has_file ".prettierrc.json"
+                or utils.root_has_file ".prettierrc.js"
+            end,
+          })
+        end,
+      }
     end,
   },
   {
