bc8bcdcd7405b4260ab7f3570b88adacdc23db93
kensei18 <kensei.sakai@gmail.com>
2024-08-08 15:30:39 +0900

    update action to search LSP symbols

--- lua/config/commands/telescope.lua
+++ lua/config/commands/telescope.lua
@@ -22,4 +22,38 @@ function M.with_search_dirs(f, opts)
 
   vim.ui.input({ prompt = "Directories", completion = "file" }, on_confirm)
 end
+
+function M.search_lsp_symbols()
+  local builtin = require "telescope.builtin"
+  local symbol_kinds = {
+    ["all"] = "all",
+    ["interface"] = "interface",
+    ["struct"] = "struct",
+    ["class/type"] = "class",
+    ["function"] = "function",
+    ["method"] = "method",
+    ["variable"] = "variable",
+    ["constant"] = "constant",
+    ["field"] = "field",
+    ["other"] = "other",
+  }
+  local symbol_keys =
+    { "all", "interface", "struct", "class/type", "function", "method", "variable", "constant", "field", "other" }
+
+  vim.ui.select(symbol_keys, { prompt = "Select symbol kinds" }, function(selected)
+    if selected == "all" then
+      builtin.lsp_dynamic_workspace_symbols()
+    elseif selected == "other" then
+      vim.ui.input(
+        { prompt = "Enter symbol kind" },
+        function(input) builtin.lsp_dynamic_workspace_symbols { symbols = input } end
+      )
+    elseif selected == "" or selected == nil then
+      return
+    else
+      builtin.lsp_dynamic_workspace_symbols { symbols = symbol_kinds[selected] }
+    end
+  end)
+end
+
 return M
