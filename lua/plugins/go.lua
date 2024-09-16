8939031bcc43b98eb00b5065b022b4a5370482a2
kensei18 <kensei.sakai@gmail.com>
2024-09-16 12:38:00 +0900

    stop installing go packages in updating go plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -10,7 +10,7 @@ return {
     config = function() require("go").setup() end,
     event = { "CmdlineEnter" },
     ft = { "go", "gomod" },
-    build = ':lua require("go.install").update_all_sync()',
+    -- build = ':lua require("go.install").update_all_sync()',
   },
   {
     "edolphin-ydf/goimpl.nvim",
