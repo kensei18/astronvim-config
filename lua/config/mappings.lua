42bfe311aa24460eee06b0a14e66ec01a915b4b5
kensei18 <kensei.sakai@gmail.com>
2025-03-12 14:49:21 +0900

    update keymap to find buffers

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -28,7 +28,8 @@ local maps = {
 
     -- buffer
     ["<Leader>bt"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    ["<Leader>bb"] = { ":Telescope buffers<cr>", desc = "Find buffers" },
+    -- ["<Leader>bb"] = { ":Telescope buffers<cr>", desc = "Find buffers" },
+    ["<Leader>h"] = { ":Telescope buffers<cr>", desc = "Find buffers" },
 
     -- sessions
     ["<Leader>r"] = { function() require("resession").load(nil, { dir = "dirsession" }) end, desc = "Load a session" },
