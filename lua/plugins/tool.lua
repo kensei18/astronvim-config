1b71e7f39963712ca683d6c31bb10194b28760c5
kensei18 <kensei.sakai@gmail.com>
2025-03-13 15:57:27 +0900

    update noice.nvim config

--- lua/plugins/tool.lua
+++ lua/plugins/tool.lua
@@ -95,6 +95,9 @@ return {
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
+        hover = {
+          enabled = false,
+        },
       },
       -- you can enable a preset for easier configuration
       presets = {
